ec0db2dc7cfb5fa037444efcbf1ee317e7a5e723
Daniel Neis Araujo <danielneis@gmail.com>
2014-06-09 17:17:51 -0300

    splitting 40% testing

--- mahout-start.sh
+++ mahout-start.sh
@@ -1,2 +1,3 @@
 ~/mahout/bin/mahout seqdirectory -i ./mahout-input/all/ -o mahout-output/seq
 ~/mahout/bin/mahout seq2sparse -i mahout-output/seq/ -o mahout-output/vectors
+~/mahout/bin/mahout split -i mahout-output/vectors/tfidf-vectors/ --trainingOutput mahout-output/train-vectors --testOutput mahout-output/test-vectors --randomSelectionPct 40 --overwrite --sequenceFiles -xm sequential
